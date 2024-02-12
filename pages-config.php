8dabb192991cf74770a193174ab0ade7e0cc0d1c
montionprojectr <montionprojectr@gmail.com>
2024-02-12 14:24:32 +0700

    pwd

--- pages-config.php
+++ pages-config.php
@@ -1,6 +1,8 @@
 <?php 
  
-$koneksi = mysqli_connect("localhost","root","","db_absensi_sp1");
+// $koneksi = mysqli_connect("localhost","root","","db_absensi_sp1");
+
+$koneksi = mysqli_connect("localhost","smkm2925_absensi","absen123456789","smkm2925_presensi_el");
  
 // Check connection
 if (mysqli_connect_errno()){
