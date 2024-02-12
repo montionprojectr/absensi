2830cef8897170b9a7b5c4048f7cd945d592160d
montionprojectr <montionprojectr@gmail.com>
2024-02-12 14:32:01 +0700

    up query

--- pages-adm-jurnal-dkelasa.php
+++ pages-adm-jurnal-dkelasa.php
@@ -5,11 +5,11 @@ echo "<option value=''>Pilih Rombel</option>";
 
 $id_kelas = $_POST['id_kelas'];
 
-$query = mysqli_query($koneksi, "SELECT id_rombel, x.id_kelas, name_kelas, x.id_jurusan, name_jurusan, rombel, CONCAT_WS(' - ', name_kelas, singkat_jurusan, rombel) AS kelas 
-FROM tb_kel_jur_rombel X 
-INNER JOIN tb_kelas Y ON y.id_kelas = x.id_kelas
-INNER JOIN tb_jurusan z ON z.id_jurusan = x.id_jurusan
-WHERE x.id_kelas = '".$id_kelas."'");
+$query = mysqli_query($koneksi, "select id_rombel, x.id_kelas, name_kelas, x.id_jurusan, name_jurusan, rombel, concat_ws(' - ', name_kelas, singkat_jurusan, rombel) as kelas 
+from tb_kel_jur_rombel x 
+inner join tb_kelas y on y.id_kelas = x.id_kelas
+inner join tb_jurusan z on z.id_jurusan = x.id_jurusan
+where x.id_kelas = '".$id_kelas."'");
 while ($data = mysqli_fetch_array($query)) {
 	echo "<option value='".$data['id_rombel']."'>".$data['kelas']."</option>";
 }
